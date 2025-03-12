a23f72ff689a507bf01d220f8c2917b91f07062e
Pepe <173433735+pepe-onche@users.noreply.github.com>
2025-03-12 23:09:54 +0100

    fix

--- onchebot/examples/tamagonche.py
+++ onchebot/examples/tamagonche.py
@@ -8,9 +8,9 @@ from supabase import create_client, Client
 def create(id: str, user: User, topic_id: int, supabase_url: str, supabase_key: str) -> Bot:
     supabase: Client = create_client(supabase_url, supabase_key)
 
-    counter = onchebot.add_bot(id, user, topic_id, default_state={"count": 0})
+    tamagonche = onchebot.add_bot(id, user, topic_id, default_state={"count": 0})
 
-    @counter.on_message()
+    @tamagonche.on_message()
     async def feed(msg: Message):  # pyright: ignore[reportUnusedFunction]
         response = (
             supabase.table("pets")
@@ -18,15 +18,12 @@ def create(id: str, user: User, topic_id: int, supabase_url: str, supabase_key: 
             .eq("id", 1)
             .execute()
         )
-        print(response)
-        if "data" not in response:
-            return
-        if len(response["data"]) <= 0:
+        if len(response.data) <= 0:
             return
 
-        status = str(response["data"][0]["status"])
-        food_level = int(response["data"][0]["food"])
-        max_food = int(response["data"][0]["max_food"])
+        status = str(response.data[0]["status"])
+        food_level = int(response.data[0]["food"])
+        max_food = int(response.data[0]["max_food"])
 
         # If dead
         if food_level <= 0 or status == "dead":
@@ -42,6 +39,5 @@ def create(id: str, user: User, topic_id: int, supabase_url: str, supabase_key: 
             .insert({"type": "feed", "username": msg.username, "pet_id": 1})
             .execute()
         )
-        print(response)
 
-    return counter
+    return tamagonche
