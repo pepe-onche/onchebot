08865aa3f9a375216544ed198e49ab264fe44d49
Pepe <173433735+pepe-onche@users.noreply.github.com>
2025-03-23 02:49:47 +0100

    rm tamagonche

--- onchebot/examples/__init__.py
+++ onchebot/examples/__init__.py
@@ -3,7 +3,6 @@ from onchebot.examples.counter import create as create_counter
 from onchebot.examples.hangman import create as create_hangman
 from onchebot.examples.pingpong import create as create_pingpong
 from onchebot.examples.repeater import create as create_repeater
-from onchebot.examples.tamagonche import create as create_tamagonche
 
 __all__ = [
     "create_apod",
@@ -11,5 +10,4 @@ __all__ = [
     "create_hangman",
     "create_pingpong",
     "create_repeater",
-    "create_tamagonche",
 ]
